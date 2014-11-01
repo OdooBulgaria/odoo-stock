b21ffc39d5265c91668e628530f798cf76b49d7a
Roberto Lizana <robertolizana@trey.es>
2014-11-01 23:01:14 +0100

    [FIX] PEP8

--- stock_account_extend/models/stock.py
+++ stock_account_extend/models/stock.py
@@ -32,28 +32,35 @@ class Picking(models.Model):
         cr, uid, context = self.env.args
         self.invoice_total = 0
         for move in self.move_lines:
-            if move.invoice_state == '2binvoiced' and move.state != 'cancel' and not move.scrapped:
+            if (move.invoice_state == '2binvoiced'
+                    and move.state != 'cancel'
+                    and not move.scrapped):
                 if move.procurement_id and move.procurement_id.sale_line_id:
                     sale_line = move.procurement_id.sale_line_id
                     if move.product_id.id != sale_line.product_id.id:
-                        price_unit = self.pool['product.pricelist'].price_get(
+                        prices = self.pool['product.pricelist'].price_get(
                             cr, uid,
                             [sale_line.order_id.pricelist_id.id],
                             move.product_id.id, move.product_uom_qty or 1.0,
                             sale_line.order_id.partner_id,
-                            context=self.env.context)[sale_line.order_id.pricelist_id.id]
+                            context=self.env.context)
+                        price_unit = prices[sale_line.order_id.pricelist_id.id]
                     else:
                         price_unit = sale_line.price_unit
 
-                    price_unit = (price_unit * (1 - (sale_line.discount or 0.0) / 100.0))
+                    price_unit = (price_unit *
+                                  (1 - (sale_line.discount or 0.0) / 100.0))
 
                     taxes = self.pool.get('account.tax').compute_all(
                         cr, uid, sale_line.tax_id,
                         price_unit, move.product_uos_qty, move.product_id,
                         sale_line.order_id.partner_id)['taxes']
-                    self.invoice_total += price_unit * move.product_uos_qty + sum(t.get('amount', 0.0) for t in taxes)
+                    self.invoice_total += price_unit * move.product_uos_qty + \
+                        sum(t.get('amount', 0.0) for t in taxes)
                 else:
-                    product = move.product_id.with_context(currency_id=move.company_id.currency_id.id)
-                    price_unit = move.product_id.list_price and move.product_id.list_price or \
-                                 product.price_get('standard_price')[move.product_id.id]
+                    product = move.product_id.with_context(
+                        currency_id=move.company_id.currency_id.id)
+                    price_unit = move.product_id.list_price and \
+                        move.product_id.list_price or \
+                        product.price_get('standard_price')[move.product_id.id]
                     self.invoice_total += price_unit * move.product_uos_qty
